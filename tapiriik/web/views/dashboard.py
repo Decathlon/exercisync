cd46694ab15eb34637a7b4f11c1a521698d8294a
kevforget <kevin.forget.partner@decathlon.com>
2023-02-17 15:02:48 +0100

    fix: removed mobile workflow to avoid bug

--- tapiriik/web/views/dashboard.py
+++ tapiriik/web/views/dashboard.py
@@ -5,14 +5,14 @@ from tapiriik.services import Service
 
 @ensure_csrf_cookie
 def dashboard(req):
-    if req.user is None and req.COOKIES.get("device_support") == "mobile" or req.user is None and "mobile" in req.GET:
-        return render(req, "static/onboarding.html")
-    else:
-        if req.user is not None:
-            current_user_decathlon_connection_id = next((svc["ID"] for svc in req.user.get("ConnectedServices") if svc.get("Service") == "decathlon"), None)
-            if current_user_decathlon_connection_id is not None:
-                current_user_dkt_svc_record = Service.GetServiceRecordByID(current_user_decathlon_connection_id)
-                if current_user_dkt_svc_record is not None and current_user_dkt_svc_record.HasAuthSyncError():
-                    User.Logout(req)
-                    return redirect(current_user_dkt_svc_record.Service.UserAuthorizationURL)
-        return render(req, "dashboard.html")
+    # if req.user is None and req.COOKIES.get("device_support") == "mobile" or req.user is None and "mobile" in req.GET:
+    #     return render(req, "static/onboarding.html")
+    # else:
+    if req.user is not None:
+        current_user_decathlon_connection_id = next((svc["ID"] for svc in req.user.get("ConnectedServices") if svc.get("Service") == "decathlon"), None)
+        if current_user_decathlon_connection_id is not None:
+            current_user_dkt_svc_record = Service.GetServiceRecordByID(current_user_decathlon_connection_id)
+            if current_user_dkt_svc_record is not None and current_user_dkt_svc_record.HasAuthSyncError():
+                User.Logout(req)
+                return redirect(current_user_dkt_svc_record.Service.UserAuthorizationURL)
+    return render(req, "dashboard.html")
